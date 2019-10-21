93f72e03f02a9269280d6ad95bb6bde54560d513
Manvi Shah <manvi.shah@somaiya.edu>
2019-10-21 23:31:07 +0530

    signup with issues

---
+++ login.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <title>
+            Intelligent Tourist System
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/signup.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta charset = "utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://kit.fontawesome.com/yourcode.js"></script>
+    </head>
+
+<body>
+    <header>
+        <nav id="header">
+            <div class="container">
+                <a href="#" class="pull-left visible-md visible-lg"></a>
+                <div id="heading-name">
+                    <h1>Outlines</h1>
+                    <h4>journeys beyond the map</h4>
+                </div>
+                <div class="topnav" id="myTopnav">
+                    <a href="signup.php" class="active">Sign Up</a>
+                    <a href="contact.html">Contact</a>
+                    <a href="gallery.html">Gallery</a>
+                    <a href="services.html">Services</a>
+                    <a href="index.html">Home</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div id="signup-content">
+        <h1>Login</h1>
+    </div>
+    <div class="container" style="text-align: center;">
+        <form>
+            <input type="email" id="email" name="email" placeholder="Email" required><br>
+            <input type="text" id="password" name="password" placeholder="Password" required><br>
+            <input type="submit" value="Submit">
+        </form>
+        <p style="color: #4A5868; font-family: Georgia, 'Times New Roman', Times, serif;">
+            Don't have an account? <a href="signup.php">Sign Up now.</a>
+        </p>
+    </div>
+    <br>
+    <footer class="panel-footer">
+        <div class="container">
+            <div id="contactno">
+                <p>Mo : +9102221642356</p>
+                <p>Email : info@outlines.com</p>
+            </div>
+            <div class="row">
+                <div class="column" id="copyright">&copy; 2019 Outlines - All rights reserved</div>
+                <div class="column" id="sociallogo">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                    <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                </div>
+            </div>
+        </div>
+    </footer> 
+
+</body>
+
+</html>
