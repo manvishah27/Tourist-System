aff94c57a17d5d0f1041a6fbb36e1bed29179741
Manvi Shah <manvi.shah@somaiya.edu>
2019-11-04 23:03:02 +0530

    Packages displayed

--- header.php
+++ header.php
@@ -1,101 +1,73 @@
-<?php
-  session_start();
+<?php 
   require_once 'db/config.php';
-  error_reporting(0);
   ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>
-            Intelligent Tourist System
-        </title>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-	    <meta charset = "utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
-	</head>
-
-    <body id="home" class="scrollspy">
-  <!-- Navbar -->
-  <div class="navbar-fixed">
-    <nav class="grey darken-3">
-      <div class="container">
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo" style="letter-spacing: 4px;">Outlines</a>
-          <a href="#" data-target="mobile-nav" class="sidenav-trigger">
-            <i class="material-icons">menu</i>
-          </a>
-          <ul class="right hide-on-med-and-down">
-            <li>
-              <a href="index.php">Home</a>
-            </li>
-            <li>
-              <a href="services.php">Services</a>
-            </li>
-            <li>
-              <a href="gallery.php">Gallery</a>
-            </li>
-            <li>
-              <a href="contact.php">Contact</a>
-            </li>
-
-            <?php if(isset($_SESSION['login']))
-            {
-            ?>
-            <li>
-              <a href="logout.php">Logout</a>
-            </li>
-            <?php 
-              }
-              else {
-            ?>
-            <li>
-              <a href="login.php">Login</a>
-            </li>
-            <?php
-              }
-            ?>
-            
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </div>
-  <ul class="sidenav" id="mobile-nav">
-    <li>
-      <a href="index.php">Home</a>
-    </li>
-    <li>
-      <a href="services.php">Services</a>
-    </li>
-    <li>
-      <a href="gallery.php">Gallery</a>
-    </li>
-    <li>
-      <a href="contact.php">Contact</a>
-    </li>
-    
-     <?php if(isset($_SESSION['login']))
-            {
-            ?>
-            <li>
-              <a href="logout.php">Logout</a>
-            </li>
-            <?php 
-              }
-              else {
-            ?>
-            <li>
-              <a href="login.php">Login</a>
-            </li>
-            <?php
-              }
-            ?>
-
-  </ul>
-
-</body>
-</html>
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/jquery-ui.css">				
+			<link rel="stylesheet" href="css/nice-select.css">							
+			<link rel="stylesheet" href="css/animate.min.css">
+			<link rel="stylesheet" href="css/owl.carousel.css">				
+			<link rel="stylesheet" href="css/main.css">			
+			<link rel="stylesheet" href="css/packages.css">
+		</head>
+	<body>
+<header id="header">
+				<div class="header-top">
+					<!--<div class="container">
+			  		<div class="row align-items-center">
+			  			<div class="col-lg-6 col-sm-6 col-6 header-top-right">
+							<div class="header-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-youtube"></i></a>
+								<a href="#"><i class="fa fa-linkedin"></i></a>
+							</div>
+			  			</div>
+			  		</div>			  					
+					</div>-->
+				</div>
+				<div class="container main-menu">
+					<div class="row align-items-center justify-content-between d-flex">
+				      <div id="logo">
+				        <a href="index.html" style="letter-spacing: 3px; color: white; font-size: 1.5em;">Outlines</a>
+				      </div>
+				      <nav id="nav-menu-container">
+				        <ul class="nav-menu">
+				          <li><a href="index.html">Home</a></li>
+				          <li><a href="about.html">About</a></li>
+				          <li><a href="packages.html">Packages</a></li>
+				          <li><a href="hotels.html">Hotels</a></li>
+				          <!--<li><a href="insurance.html">Insurence</a></li>
+				          <li class="menu-has-children"><a href="">Blog</a>
+				            <ul>
+				              <li><a href="blog-home.html">Blog Home</a></li>
+				              <li><a href="blog-single.html">Blog Single</a></li>
+				            </ul>
+				          </li>	
+				          <li class="menu-has-children"><a href="">Pages</a>
+				            <ul>
+				            	  <li><a href="elements.html">Elements</a></li>
+						          <li class="menu-has-children"><a href="">Level 2 </a>
+						            <ul>
+						              <li><a href="#">Item One</a></li>
+						              <li><a href="#">Item Two</a></li>
+						            </ul>
+						          </li>					                		
+				            </ul>
+				          </li>	-->				          					          		          
+				          <li><a href="contact.html">Contact</a></li>
+				        </ul>
+				      </nav><!-- #nav-menu-container -->					      		  
+					</div>
+				</div>
+			</header><!-- #header -->
+		</body>
+		</html>
--- package-details.php
+++ package-details.php
@@ -2,7 +2,7 @@
   session_start();
   require_once 'db/config.php';
   error_reporting(0);
-if(isset($_POST['submit2']))
+/*if(isset($_POST['submit2']))
 {
 
 echo 'hi';
@@ -33,7 +33,7 @@ else
 $error="Something went wrong. Please try again";
 }
 */
-}
+
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -41,16 +41,26 @@ $error="Something went wrong. Please try again";
         <title>
             Intelligent Tourist System
         </title>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="css/packages_styles.css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-        <link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+        <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+        <link href="css/animate.css" rel="stylesheet" type="text/css" media="all">-->
         <script src="js/jquery-1.12.0.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/wow.min.js"></script>
         <script src="js/jquery-ui.js"></script>
         <link rel="stylesheet" href="css/jquery-ui.css" />
+        <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/jquery-ui.css">				
+			<link rel="stylesheet" href="css/nice-select.css">							
+			<link rel="stylesheet" href="css/animate.min.css">
+			<link rel="stylesheet" href="css/owl.carousel.css">				
+			<link rel="stylesheet" href="css/main.css">			
+			<link rel="stylesheet" href="css/packages.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 
       <meta charset = "utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -72,7 +82,25 @@ $error="Something went wrong. Please try again";
 
  <body id="home" class="scrollspy">
 
-<?php include('header.php'); ?>
+<?php 
+include('header.php');
+?>
+			  
+			<!-- start banner Area -->
+			<section class="about-banner relative">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">				
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="about-content col-lg-12">
+							<h1 class="text-white">
+								Tour Packages				
+							</h1>	
+							<!--<p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="packages.html"> Tour Packages</a></p>-->
+						</div>	
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
 
 <?php if($error){?>
 	<div class="errorWrap">
@@ -102,10 +130,10 @@ while($row=$result->fetch_assoc())
 
 
 		<div class="selectroom_top">
-			<div class="col-md-4 selectroom_left wow fadeInLeft animated" data-wow-delay=".5s">
-				<img src="images/<?php echo htmlentities($row['image']);?>" class="img-responsive" alt="">
+			<div class="col-md-4 selectroom_left" data-wow-delay=".5s">
+				<img src="img/<?php echo htmlentities($row['image']);?>" class="img-responsive" alt="">
 			</div>
-			<div class="col-md-8 selectroom_right wow fadeInRight animated" data-wow-delay=".5s">
+			<div class="col-md-8 selectroom_right" data-wow-delay=".5s">
 				<h2><?php echo htmlentities($row['name']);?></h2>
 				<p class="dow">#PKG-<?php echo htmlentities($row['package_id']); $package_id=$row['package_id'];?></p>
 				<p><b>Type :</b> <?php echo htmlentities($row['type']);?></p>
@@ -113,10 +141,11 @@ while($row=$result->fetch_assoc())
 					<p><b>Features</b> <?php echo htmlentities($row['features']);?></p>
 		
 						<div class="clearfix"></div>
-				<div class="grand">
+				<div>
 					<p>Amount</p>
-					<h3>&#x20b9; <?php echo htmlentities($row['price']); $price=$row['price']; ?></h3>
+					<h4>&#x20b9; <?php echo htmlentities($row['price']); $price=$row['price']; ?></h4>
 				</div>
+				<br>
 			</div>
 
 		<h3>Details</h3>
@@ -126,40 +155,52 @@ while($row=$result->fetch_assoc())
 <?php } ?>
 
 		<div class="selectroom_top" id="tour_details_form">
-			<h2>Travels</h2>
-			<div class="selectroom-info animated wow fadeInUp animated" data-wow-duration="1200ms" data-wow-delay="500ms" style="visibility: visible; animation-duration: 1200ms; animation-delay: 500ms; animation-name: fadeInUp; margin-top: -70px">
-				<br>
-				<br>
-			<form name="book" method="post" action="package-details.php?pkgid=<?php echo htmlentities($package_id);?>">
+			<h2>Enter your Details</h2>
+			<div class="selectroom-info" style="visibility: visible; margin-top: 30px">
+
+
+ <!--<div class="tab-pane fade show active" id="flight" role="tabpanel" aria-labelledby="flight-tab">
+								<form class="form-wrap">
+									<input type="text" class="form-control" name="name" placeholder="From " onfocus="this.placeholder = ''" onblur="this.placeholder = 'From '">									
+									<input type="text" class="form-control" name="to" placeholder="To " onfocus="this.placeholder = ''" onblur="this.placeholder = 'To '">
+									<input type="text" class="form-control date-picker" name="start" placeholder="Start " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Start '">
+									<input type="text" class="form-control date-picker" name="return" placeholder="Return " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Return '">
+									<input type="number" min="1" max="20" class="form-control" name="adults" placeholder="Adults " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Adults '">
+									<input type="number" min="1" max="20" class="form-control" name="child" placeholder="Child " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Child '">							
+									<a href="#" class="primary-btn text-uppercase">Search flights</a>									
+								</form>
+							  </div>-->
+
+
+			<form name="book" class="form-wrap" method="post" action="package-details.php">
 
 			<div class="bnr-right">
 				<label class="inputLabel">Package ID</label>
-				<input id="package_id" type="number" name="package_id" value="<?php echo $package_id; ?>" required="" readonly="">
-			</div>
+				<input id="package_id" class="form-control" type="number" name="package_id" value="<?php echo $package_id; ?>" required="">
+			</div><br>
 
 			<div class="bnr-right">
 				<label class="inputLabel">Package Price</label>
-				<input id="price" type="number" name="price" value="<?php echo $price; ?>" required="" readonly="">
-			</div>
+				<input id="price" class="form-control" type="number" name="price" value="<?php echo $price; ?>" required="">
+			</div><br>
 
 					<div class="ban-bottom">
 				<div class="bnr-right">
 				<label class="inputLabel">From</label>
-				<input class="date" id="datepicker" type="text" placeholder="yyyy-mm-dd"  name="fromDate" required="">
-			</div>
+				<input class="date form-control" id="datepicker" type="text" placeholder="yyyy-mm-dd"  name="fromDate" required="">
+			</div><br>
 			<div class="bnr-right">
 				<label class="inputLabel">To</label>
-				<input class="date" id="datepicker1" type="text" placeholder="yyyy-mm-dd" name="toDate" required="">
-			</div>
-			</div>
+				<input class="date form-control" id="datepicker1" type="text" placeholder="yyyy-mm-dd" name="toDate" required="">
+			</div><br>
 			<div class="bnr-right">
 				<label class="inputLabel">No of Travellers</label>
-				<input id="noOfPassengers" type="number" name="noOfPassengers" required="">
-			</div>
+				<input id="noOfPassengers" class="form-control"s type="number" name="noOfPassengers" required="">
+			</div><br>
 					<div class="spe">
 						<label class="inputLabel">Comment</label>
-						<input class="special" type="text" name="comment" required="">
-					</div> 
+						<input class="special form-control" type="text" name="comment" required="">
+					</div> <br>
 					<?php if(isset($_SESSION['login']))
 					{?>
 						
@@ -194,8 +235,8 @@ while($row=$result->fetch_assoc())
 	 ?>
 
 		<div class="selectroom_top" id="payment_form" style="display: none;">
-			<h2>Travels</h2>
-			<div class="selectroom-info animated wow fadeInUp animated" data-wow-duration="1200ms" data-wow-delay="500ms" style="visibility: visible; animation-duration: 1200ms; animation-delay: 500ms; animation-name: fadeInUp; margin-top: -70px">
+			<h2>Enter your Details</h2>
+			<div class="selectroom-info" data-wow-duration="1200ms" data-wow-delay="500ms" style="visibility: visible; margin-top: -70px">
 				<br>
 				<br>
 
@@ -227,32 +268,90 @@ while($row=$result->fetch_assoc())
 </div>
 <!--- /selectroom ---->
 
-<footer class="section grey darken-3 white-text">
-    <div class="container">
-        <div class="row center">
-            <div class="col s6" style="font-size: 1.2em">
-                <h5>Contact:</h5>
-                <p>Mo : +9102221642356</p>
-                <p>Email : info@outlines.com</p>
-            </div>
-            <div class="col s6">
-                <p class="flow-text">&copy; 2019 Outlines</p>
-                <a href="#" class="white-text">
-                    <i class="fab fa-facebook fa-2x"></i>
-                </a>
-                <a href="#" class="white-text">
-                    <i class="fab fa-twitter fa-2x"></i>
-                </a>
-                <a href="#" class="white-text">
-                    <i class="fab fa-linkedin fa-2x"></i>
-                </a>
-                <a href="#" class="white-text">
-                    <i class="fab fa-google-plus fa-2x"></i>
-                </a>
-            </div>
-        </div>
-    </div>
-    </footer>
+<!-- start footer Area -->		
+			<footer class="footer-area section-gap">
+				<div class="container">
+
+					<div class="row">
+						<div class="col-lg-3  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>About Agency</h6>
+								<p>
+									The world has become so fast paced that people don’t want to stand by reading a page of information, they would much rather look at a presentation and understand the message. It has come to a point 
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>Navigation Links</h6>
+								<div class="row">
+									<div class="col">
+										<ul>
+											<li><a href="#">Home</a></li>
+											<li><a href="#">Feature</a></li>
+											<li><a href="#">Services</a></li>
+											<li><a href="#">Portfolio</a></li>
+										</ul>
+									</div>
+									<div class="col">
+										<ul>
+											<li><a href="#">Team</a></li>
+											<li><a href="#">Pricing</a></li>
+											<li><a href="#">Blog</a></li>
+											<li><a href="#">Contact</a></li>
+										</ul>
+									</div>										
+								</div>							
+							</div>
+						</div>							
+						<div class="col-lg-3  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>Newsletter</h6>
+								<p>
+									For business professionals caught between high OEM price and mediocre print and graphic output.									
+								</p>								
+								<div id="mc_embed_signup">
+									<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscription relative">
+										<div class="input-group d-flex flex-row">
+											<input name="EMAIL" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+											<button class="btn bb-btn"><span class="lnr lnr-location"></span></button>		
+										</div>									
+										<div class="mt-10 info"></div>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3  col-md-6 col-sm-6">
+							<div class="single-footer-widget mail-chimp">
+								<h6 class="mb-20">InstaFeed</h6>
+								<ul class="instafeed d-flex flex-wrap">
+									<li><img src="img/i1.jpg" alt=""></li>
+									<li><img src="img/i2.jpg" alt=""></li>
+									<li><img src="img/i3.jpg" alt=""></li>
+									<li><img src="img/i4.jpg" alt=""></li>
+									<li><img src="img/i5.jpg" alt=""></li>
+									<li><img src="img/i6.jpg" alt=""></li>
+									<li><img src="img/i7.jpg" alt=""></li>
+									<li><img src="img/i8.jpg" alt=""></li>
+								</ul>
+							</div>
+						</div>						
+					</div>
+
+					<div class="row footer-bottom d-flex justify-content-between align-items-center">
+						<p class="col-lg-8 col-sm-12 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+						<div class="col-lg-4 col-sm-12 footer-social">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+							<a href="#"><i class="fa fa-behance"></i></a>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<!-- End footer Area -->	
 
 </body>
 </html>
