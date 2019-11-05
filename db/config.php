0ec9ddb0e5cb0356d74cca0ed452318a4f0f16bd
namasvishah <51482616+namasvishah@users.noreply.github.com>
2019-11-05 09:52:47 +0530

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "";
 $dbname = "intelligent_tourist_system";
  
 $conn = mysqli_connect($servername, $username, $password, $dbname);
