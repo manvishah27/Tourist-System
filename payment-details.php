7a7487057079c15acd030719bb0453c4076df5f2
namasvishah <51482616+namasvishah@users.noreply.github.com>
2019-11-05 12:16:27 +0530

    Update payment-details.php

--- payment-details.php
+++ payment-details.php
@@ -5,7 +5,7 @@
 if(isset($_POST['submit2']))
 {
 
-echo 'hi';
+
 
 $pid=intval($_GET['pkgid']);
 $email=$_SESSION['login'];
@@ -224,7 +224,21 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                     </div>
                 </div>
             </footer>
-            <!-- End footer Area -->    
+            <!-- End footer Area --> 
+            <script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="js/popper.min.js"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>			
+			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>		
+ 			<script src="js/jquery-ui.js"></script>					
+  			<script src="js/easing.min.js"></script>			
+			<script src="js/hoverIntent.js"></script>
+			<script src="js/superfish.min.js"></script>	
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>						
+			<script src="js/jquery.nice-select.min.js"></script>					
+			<script src="js/owl.carousel.min.js"></script>							
+			<script src="js/mail-script.js"></script>	
+			<script src="js/main.js"></script>	   
 
     </body>
     </html>
